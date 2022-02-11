268206eb195ea453e073add24efc13ec82bb4c3c
Ismael Treviño <ismolahiena@gmail.com>
2022-02-11 17:45:47 +0100

    Ahora si: todo terminado

--- Base de datos/escuela.sql
+++ Base de datos/escuela.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-02-2022 a las 17:31:44
+-- Tiempo de generación: 11-02-2022 a las 17:43:47
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
