692ad3f746d42a4a4b416444a6f596a834eda886
Ismael Treviño <ismolahiena@gmail.com>
2022-02-11 17:43:10 +0100

    README cambiado

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # DWES-Unidad-3
-Trabajo final de la unidad 3
+## Trabajo final de la unidad 3
+### Requisitos para que el proyecto funcione:
+La base de datos con la que se trabaja es escuela. Contiene 2 tablas:
+#### Alumnos
+id - nombre - apellidos - dni - fechaNacimiento - idTipoVia - nombreVia - numeroVia - localidad - telefono
+#### Usuarios
+id - usuario - contraseña - fecha_alt - permisos - true
+
+Esta ultima tabla contiene un usuario llamado root, de contraseña root
+### Que encontrarás
+Desde esta base de datos, se puede crear usuarios (siempre que no existan ya)
+Iniciar sesion (siempre que pongas bien la contraseña y el usuario)
+Entrar a la base de datos.
+#### Si no tienes permisos podrás:
+Hacer una consulta a la base de datos mediante un formulario para ver lo usuarios
+
+#### Si tienes permisos podrás:
+Además de lo anterior, podrás introducir alumnos. Para rellenaras un formulario.
+Editar los alumnos existentes. Para ello podrás hacer una consulta a la base de datos que generará una tabla editable con los alumnos buscados
+Eliminar alumnos. Para ello tendrás que hacer una consulta a la base de datos que generará una tabla con un chekbox para cada usuario
+Ver y modificar los a los usuarios. Para ello tendrás que hacer una consulta a la base de datos, que generará una tabla editable. Útil para darle permisos a los usuario, ya que al principio solo root tiene permisos. Tambiém permitirá cambiar el tema, teniendo 4 para elegir. Al cambiar el tema se cambiará el fondo y los colores que se usan de acento.
---
+++ escuela.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-02-2022 a las 17:31:44
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `escuela`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `id` int(10) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
+  `dni` varchar(50) NOT NULL,
+  `fechaNacimiento` varchar(50) NOT NULL,
+  `idTipoVia` varchar(50) NOT NULL,
+  `nombreVia` varchar(50) NOT NULL,
+  `numeroVia` varchar(50) NOT NULL,
+  `localidad` varchar(50) NOT NULL,
+  `telefono` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(60) NOT NULL,
+  `contraseña` varchar(255) NOT NULL,
+  `fecha_alt` varchar(60) NOT NULL,
+  `permisos` varchar(60) NOT NULL,
+  `tema` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `contraseña`, `fecha_alt`, `permisos`, `tema`) VALUES
+(14, 'root', '$2y$10$sbDZLIQgAGYlV5MzRhI1JOffWNR4mrkqGKDvpVmgTCwEWxIshzUN2', '11/2/2022', 'true', 'light');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
